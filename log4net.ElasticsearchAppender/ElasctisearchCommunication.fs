5f7ef6ff37e458093f46545002cec4898164a74f
Michal Duda <michal.duda@ncr.com>
2016-11-23 21:40:37 +0100

    cleaning code in ES communication

--- log4net.ElasticsearchAppender/ElasctisearchCommunication.fs
+++ log4net.ElasticsearchAppender/ElasctisearchCommunication.fs
@@ -22,32 +22,10 @@ module Communication =
         serialized.ToString()
 
     let sendFunction() : PostFunction<HttpStatusCode> = 
-//        fun (connectionInfo : ConnectionInfo) (events : LoggingEvent[]) ->
-//            let req = WebRequest.CreateHttp (connectionInfo.GetUrlString())
-//            req.Method <- "POST"
-//            req.ProtocolVersion <- HttpVersion.Version10
-//            let postBytes = buildContent events |> System.Text.Encoding.ASCII.GetBytes
-//            req.ContentLength <- postBytes.LongLength
-//            req.ContentType <- "application/xml; charset=utf-8"
-//            req.Timeout <- 1500
-//            async{
-//                Console.WriteLine("About to send events")
-//                use reqStream = req.GetRequestStream()
-//                do! reqStream.WriteAsync(postBytes, 0, postBytes.Length) |> Async.AwaitIAsyncResult |> Async.Ignore
-//                reqStream.Close()
-//                use! res = req.AsyncGetResponse()
-//                let httpRes = res :?> HttpWebResponse
-//                return httpRes.StatusCode
-//            }
+
         let httpClient = new HttpClient()
         fun (connectionInfo : ConnectionInfo) (events : LoggingEvent[]) ->
-         
-//            let request = new HttpRequestMessage(HttpMethod.Post, connectionInfo.GetUrlString())
-//            request.Content <- new StringContent(buildContent events)
-//            request.Method <- HttpMethod.Post
-            Console.WriteLine("About to send events")
             let task = httpClient.PostAsync(connectionInfo.GetUrlString(),(new StringContent(buildContent events)), (new CancellationTokenSource(2000)).Token)
-            Console.WriteLine("After send events")
             Async.FromContinuations(fun (cont, econt, ccont) -> 
                 task.ContinueWith(fun (t:Task<HttpResponseMessage>) -> 
                     match t with

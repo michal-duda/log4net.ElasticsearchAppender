8c498e36e31daa17d4f28325b60bc781f9974725
Michal Duda <michal.duda@ncr.com>
2016-11-25 22:03:49 +0100

    code cleaning, refactoring

--- log4net.ElasticsearchAppender/Appender.fs
+++ log4net.ElasticsearchAppender/Appender.fs
@@ -1,7 +1,5 @@
 ﻿namespace log4net.ElasticsearchAppender
 
-//module Implementation =
-
     open log4net
     open log4net.Appender
     open log4net.Core
@@ -11,7 +9,7 @@
     open System.Threading.Tasks
     open System.Collections.Generic
 
-    module private InternalImpl = 
+    module Helpers = 
         open Polly
         open System.Net.Http
 
@@ -28,77 +26,43 @@
                     dictionary.["Index"]
 
             let dictionary = connectionString.Split(';') |> Array.map (fun s -> splitKeyValue s) |> dict
-            {
-                Server="http://" + dictionary.["Server"];
-                Port=Int32.Parse(dictionary.["Port"]);
-                Bulk = bufferSize > 0;
-                Index=indexName dictionary
-            }
-          
-// *************** Using polly now  
-//        type Result =
-//            | Success
-//            | Failure of string
-//        
-//        type CircuitBreakerState private (maxFailCount : int, resetTimeSpan: TimeSpan, failCount: int, openTime: DateTime) = 
-//                
-//                let isClosed = failCount < maxFailCount
-//                let isReadyForReset = (DateTime.Now - openTime) > resetTimeSpan
-//
-//                let prepareReturnValue res = 
-//                    match res with 
-//                    | Choice1Of2 _ -> 
-//                        if isReadyForReset then (Success, CircuitBreakerState(maxFailCount, resetTimeSpan)) else (Success, CircuitBreakerState(maxFailCount, resetTimeSpan, failCount, openTime))
-//                    | Choice2Of2 ex -> 
-//                        let newFailCount = failCount + 1
-//                        if newFailCount > maxFailCount then 
-//                            (Failure(ex.ToString()), CircuitBreakerState(maxFailCount, resetTimeSpan, newFailCount, DateTime.Now)) 
-//                        else
-//                            (Failure(ex.ToString()), CircuitBreakerState(maxFailCount, resetTimeSpan, newFailCount, openTime)) 
-//
-//                new(maxFailCount : int, resetTimeSpan: TimeSpan) = CircuitBreakerState(maxFailCount, resetTimeSpan, 0, DateTime.MinValue) 
-//
-//                member x.Execute func = async {
-//                    if isClosed || isReadyForReset then
-//                        let! res = func() |> Async.Catch
-//                        return prepareReturnValue res
-//                    else
-//                        return (Failure("Circuit Breaker Open"), CircuitBreakerState(maxFailCount, resetTimeSpan, failCount, openTime)) 
-//                }
-                
-        let sendAgent (sendFunction : PostFunction<'a>) (connectionInfoProvider : unit -> ConnectionInfo) (internalErrorHandling : string -> unit) = 
-            MailboxProcessor.Start(fun inbox-> 
-                
-                let resetTimeSpan = TimeSpan.FromMinutes(1.0)
-                let maxFailureCount = 3
-                // the message processing function
-                let rec messageLoop = async { // (cirtuitBreaker : CircuitBreakerState) = async {
-        
-                    // read a message
-                    let! msg = inbox.Receive()
-                    let! res = sendFunction (connectionInfoProvider()) msg |> Async.Catch
-                    // process a message
-//                    let! res = cirtuitBreaker.Execute( fun() -> sendFunction (connectionInfoProvider()) msg )
-                
-                    match res with 
-                        | Choice1Of2 _ -> ()
-                        | Choice2Of2(ex) -> 
-                            internalErrorHandling(String.Format("Elasticsearch Appender fail: {0}", ex))
 
-                    return! messageLoop
-                }
-                // start the loop 
-                messageLoop // (CircuitBreakerState(maxFailureCount, resetTimeSpan))
-            )
-        
+            { Server="http://" + dictionary.["Server"]; Port=Int32.Parse(dictionary.["Port"]); Bulk = bufferSize > 0; Index=indexName dictionary }
+
         let policy internalErrorHanling = Policy.Handle<Exception>()
-                                            .CircuitBreakerAsync(2, TimeSpan.FromMinutes(1.0), (fun ex span -> 
-                                                Console.WriteLine("Failures during sending logs to Elasticsearch. Opening circuit breaker. {0}")), 
+                                            .CircuitBreakerAsync(3, TimeSpan.FromMinutes(1.0), (fun ex span -> 
+                                                internalErrorHanling(sprintf "Failures during sending logs to Elasticsearch. Opening circuit breaker. %O" ex)), 
                                                 (fun () -> internalErrorHanling("Cirtuit breaker reset.")))
         
         let sendFuncWithCircuitBreaker sendFunction internalErrorHanling = 
             let p = policy internalErrorHanling
             fun uri events -> async { return! p.ExecuteAsync(fun() -> sendFunction uri events |> Async.StartAsTask) |> Async.AwaitTask }
+    
+    module ProcessingAgents =       
+        
+          
+        let mailboxProcessor (sendFunction : PostFunction<'a>) (connectionInfoProvider : unit -> ConnectionInfo) (internalErrorHandling : string -> unit) = 
+            MailboxProcessor.Start(fun inbox-> 
+                
+                let resetTimeSpan = TimeSpan.FromMinutes(1.0)
+                let maxFailureCount = 3
+
+                let rec messageLoop = async { // (cirtuitBreaker : CircuitBreakerState) = async {
+        
+                    let! msg = inbox.Receive()
+                    let! res = sendFunction (connectionInfoProvider()) msg |> Async.Catch
+                
+                    match res with 
+                        | Choice1Of2 _ -> return! messageLoop
+                        | Choice2Of2(ex) -> 
+                            internalErrorHandling(sprintf "Elasticsearch Appender fail: %O" ex)
+                            return! messageLoop                    
+                }
+
+                messageLoop
+            )
+        
+        
 
     type Appender(sendFunction : PostFunction<'a>) = 
         inherit BufferingAppenderSkeleton()
@@ -106,7 +70,7 @@
         let internalLogging = base.ErrorHandler.Error
         let mutable connectionInfo = {Server="";Index="";Port=0;Bulk=false}
 
-        let agent = InternalImpl.sendAgent (InternalImpl.sendFuncWithCircuitBreaker sendFunction internalLogging) (fun () -> connectionInfo) (fun msg -> internalLogging(msg))
+        let agent = ProcessingAgents.mailboxProcessor (Helpers.sendFuncWithCircuitBreaker sendFunction internalLogging) (fun () -> connectionInfo) (fun msg -> internalLogging(msg))
 
         new() = Appender(Communication.sendFunction())
 
@@ -119,7 +83,7 @@
             if String.IsNullOrEmpty(this.ConnectionString) then
                 base.ErrorHandler.Error("ConnectionString is null or empty");
             else
-                connectionInfo <- InternalImpl.parseConnectionString this.ConnectionString this.BufferSize
+                connectionInfo <- Helpers.parseConnectionString this.ConnectionString this.BufferSize
 
         override this.SendBuffer (events : LoggingEvent[]) = agent.Post events
             
